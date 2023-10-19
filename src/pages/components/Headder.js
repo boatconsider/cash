d34aef75bcd5424cf6e26462b5bc813530a7cca7
boatconsider <boatzarock01@gmai.com>
2023-10-19 13:56:01 +0700

    edit main

--- src/pages/components/Headder.js
+++ src/pages/components/Headder.js
@@ -19,14 +19,14 @@ export default function Headder() {
   const router = useRouter();
   return (
     <div>
-      <div className="grid grid-cols-1 grid-rows-1 bg-gradient-to-r from-cyan-200 to-blue-600 p-5">
+      <div className="grid grid-cols-1 grid-rows-1 bg-gradient-to-t from-amber-200 to-blue-300 p-3">
         <div className="flex flex-row items-center ml-3 text-[#fff] text-xl justify-between">
           <div
-            className="flex flex-row items-center cursor-pointer text-blue-500"
+            className="flex flex-row items-center cursor-pointer text-white"
             onClick={() => router.push("/")}
           >
-            <RiComputerLine />
-            <h1 className="pl-1">HardWare</h1>
+    
+            <h1 className="pl-1">RSM</h1>
           </div>
           <div className="flex items-center flex-row mr-3 outline-none border-none md:m-0">
             <div className="flex md:hidden" onClick={toggleMobileNav}>
@@ -34,32 +34,32 @@ export default function Headder() {
             </div>
             <div className="max-[650px] hidden md:flex md:p-0  ">
               <button
-                className="text-[#fff] hover:bg-[#06b6d4] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
+                className="bg-white text-[#333] hover:bg-[#333] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
                 onClick={() => router.push("/")}
               >
-                <AiOutlineHome />
+            
                 หน้าหลัก
               </button>
               <button
-                className="text-[#fff] hover:bg-[#06b6d4] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
+                className="bg-white text-[#333] hover:bg-[#333] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
                 onClick={() => router.push("/shop")}
               >
-                <AiOutlineShoppingCart />
-                ร้านค้า
+              
+              แจ้งปัญหา SDO
               </button>
               <button
-                className="text-[#fff] hover:bg-[#06b6d4] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
+                className="bg-white text-[#333] hover:bg-[#333] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
                 onClick={() => router.push("/login")}
               >
-                <AiOutlineLogin />
-                เข้าสู่ระบบ
+             
+             แจ้งปัญหา PDC/DC
               </button>
               <button
-                className="text-[#fff] hover:bg-[#06b6d4] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
+                className="bg-white text-[#333] hover:bg-[#333] hover:text-[#fff] p-1 mr-2 rounded-md flex items-center outline-none"
                 onClick={() => router.push("/signup")}
               >
-                <TbLogin />
-                สมัครสมาชิก
+
+              แจ้งปัญหา VANSEll
               </button>
             </div>
           </div>
@@ -71,29 +71,26 @@ export default function Headder() {
             className="mt-3 hover:text-[#06b6d4] flex items-center"
             onClick={() => router.push("/")}
           >
-            <AiOutlineHome />
+        
             หน้าหลัก
           </button>
           <button
             className="mt-3 hover:text-[#06b6d4] flex items-center"
             onClick={() => router.push("/shop")}
           >
-            <AiOutlineShoppingCart />
-            ร้านค้า
+      แจ้งปัญหา SDO
           </button>
           <button
             className="mt-3 hover:text-[#06b6d4] flex items-center"
             onClick={() => router.push("/login")}
           >
-            <AiOutlineLogin />
-            เข้าสู่ระบบ
+                    แจ้งปัญหา PDC/DC
           </button>
           <button
             className="mt-3 hover:text-[#06b6d4] flex items-center"
             onClick={() => router.push("/signup")}
           >
-            <TbLogin />
-            สมัครสมาชิก
+         แจ้งปัญหา VANSEll
           </button>
         </div>
       )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,99 +4,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchData = async () => {
-    const url =
-      "https://script.google.com/macros/s/AKfycbyEtdhDysTqbR_ZMPpt3vDOaS4LMXDmrvfHRSeAHuZjIlEFfj3dKJCztaF4ErR8ZlMW-w/exec?action=getUsers";
-    const res = await axios.get(url);
-    setData(res.data);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const skeletonDivs = Array.from({ length: 12 }, (_, index) => (
-    <div
-      key={index}
-      className="bg-[#fff] w-[500px] h-[400px] max-[650px]:w-full flex flex-wrap justify-center items-center mr-2 mt-2 animate-wave"
-    >
-      {/* Render the skeleton loading UI */}
-    </div>
-  ));
-
-  const dataDivs = data.slice(0, 12).map((item, index) => {
-    return (
-      <div
-        key={index}
-        className="bg-[#f2f3f5] w-[600px] h-[450px] max-[650px]:w-full flex flex-wrap justify-center items-center mr-2 mt-2 max-[650px]:bg-[#f2f3f5] max-[650px]:mr-0"
-      >
-        <div className="bg-[#f2f3f5] w-[300px] h-[300px] max-[650px]:w-[300px] max-[650px]:h-[300px]">
-          <img src={item.images} alt="" className=" object-cover" />
-        </div>
-        <div>
-          <h1>{item.header}</h1>
-          <div className="flex flex-row justify-between items-center">
-            <h1>{item.price} บาท</h1>
-            <h1>ประกัน {item.guarantee}</h1>
-          </div>
-          <div className="flex justify-center items-center">
-            <button className="w-[150px] h-[50px] rounded-lg bg-blue-500 text-[#fff]">
-              {item.discount} บาท
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  });
-
-  const divs = isLoading ? skeletonDivs : dataDivs;
+  
 
   return (
-    <div className="h-[100vh] bg-slate-200">
+    <div className="h-full bg-slate-200  min[400px]:h-auto">
       <Headder />
-      <div className=" w-full h-[1200px] mt-2  ">
-        <div className="grid grid-cols-[800px_minmax(0px,_1fr)] grid-row-[800px_minmax(200px,_1fr)] gap-x-2 gap-y-2 max-[650px]:grid-row-1 max-[650px]:grid-cols-1 max-[820px]:grid-cols-[450px_minmax(0px,_1fr)] max-[1024px]:grid-cols-[500px_minmax(0px,_1fr)]">
-          <div className="w-full bg-[#fff] h-[550px] ">
-            <img
-              src="https://cdn.shopify.com/s/files/1/0624/7949/9512/files/graphicscards_1024x1024.jpg?v=1648720902"
-              alt=""
-              className=" w-full h-full"
-            />
-          </div>
-          <div className="w-full bg-[#fff] h-[550px]">
-            <img
-              src="https://www.appdisqus.com/wp-content/uploads/2023/01/pLiv2jeg8Fb7KdjPAyyhCJ-1200-80.jpg.webp"
-              alt=""
-              className="w-full h-full"
-            />
-          </div>
-        </div>
-        <div className="  grid grid-cols-[600px_minmax(0px,_1fr)] grid-row-[800px_minmax(200px,_1fr)] gap-x-2 gap-y-2 w-full mt-2 max-[650px]:grid-row-1 max-[650px]:grid-cols-1  max-[820px]:grid-cols-[450px_minmax(0px,_1fr)]  max-[1024px]:grid-cols-[500px_minmax(0px,_1fr)]">
-          <div className="w-full bg-[#fff] h-[550px]">
-            <img
-              src="https://secure.zortout.com/Home/DisplayProductImage?pid=14687478"
-              alt=""
-              className="h-full w-full"
-            />
-          </div>
-          <div className="w-full bg-[#fff] h-[550px]">
-            <img
-              src="https://inwfile.com/s-ge/o0fctq.jpg"
-              alt=""
-              className="h-full w-full"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center items-center flex-row pt-3 flex-wrap   ">
-          {divs}
-        </div>
-        <div className="pt-[3rem]">
-          <Footer />
-        </div>
+     <div className="flex justify-center items-center mt-5">
+      <div className=" flex justify-center items-center w-[800px] h-[500px] bg-white flex-col">
+        <h1>แจ้งปัญหาหรือสิ่งที่ต้องการแก้ไขตามนี้ได้เลย</h1>
+        <h3>1. คลิกบริษัทที่ท่านทำงานดังนี้ SDO  PDC/DC หรือปัญหาของ VANSEll</h3>
+        <h3>2. จะมีฟอร์มแก้ปัญหาสิ่งที่ต้องกรอกมีดังนี้</h3>
+
+        <h3> -ชื่อผู้แจ้งปัญหา </h3>
+
+        <h3>-รหัส PS,MAS,VANSEll</h3>
+
+        <h3> -บอกที่มาของปัญหาคร่าวๆ</h3>
+    
+
+        <h3>*ไม่ว่าจะเป็นการย้าย
+แฟ้มหรือปัญหาอื่นๆก็กรอกมาทั้งหมด</h3>
+<h3> -บอกที่มาของปัญหาคร่าวๆ</h3>
+<h3>3. แล้วกด Submit แล้วรอทางทีมไอทีรับเรื่องและทำการแก้ไข</h3>
+<h3> 4. หลังจากนั้นรอทางทีม IT Support ดำเนินงาน</h3>
+
       </div>
-    </div>
+     </div> 
+       
+        <Footer  />
+      </div>
+
   );
 }
