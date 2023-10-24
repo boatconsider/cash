da7bbf3866c3f9dc9a0c714d0d4c07189bbadfbd
boatconsider <boatzarock01@gmai.com>
2023-10-24 16:22:12 +0700

    edit form sdo pdcdc vansell

--- src/pages/van.js
+++ src/pages/van.js
@@ -1,38 +1,85 @@
+import { useState } from 'react';
 import Headder from "./components/Headder"
 import Footer from "./components/Footer"
-export default function van(){
-    return(
-        <div className="h-full bg-slate-200  min[400px]:h-auto">
-        <Headder/>
+import axios from 'axios';
 
-  <div className="flex justify-center items-center mt-5">
-        <div className=" flex  items-center w-[650px] h-[650px] bg-white flex-col rounded-lg">
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+import { useRouter } from "next/router";
+export default function van() {
+  const router = useRouter();
+  const MySwal = withReactContent(Swal);
+  const [name, setName] = useState(""); 
+  const [password, setPassword] = useState(""); 
+  const [problem, setProblem] = useState(""); 
+  const [img, setImg] = useState(""); 
+
+  const handleSubmission = () => {
+  
+    axios.post('https://node-api-u9ix.onrender.com/rsmvan', {
+      name: name,
+      passwordsell: password,
+      problem: problem,
+      img: img
+    })
+    .then((response) => {
+      const result = response.data;
+      console.log(result);
+
+      // Handle login success here
+      if (result.status === "ok") {
+        MySwal.fire({
+          html: <i>{result.message}</i>,
+          icon: "success",
+        }).then(() => {
+          router.push("/");
+        });
+      } else {
+        MySwal.fire({
+          title: <strong>Login failed.</strong>,
+          icon: "error",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error.message);
+      // Handle the error here
+    });
+  }
+
+  return (
+    <div className="h-full bg-slate-200 min-h-[400px]">
+      <Headder />
+      <div className="flex justify-center items-center mt-2">
+        <img src="itsupport.png" alt="" width={300} height={300} />
+      </div>
+      <div className="flex justify-center items-center mt-5">
+        <div className="flex items-center w-[650px] h-[650px] bg-white flex-col rounded-lg">
           <div>
-            <img src="3.png" alt="" width={150} height={100} />
+            <img src="1.png" alt="" width={150} height={100} />
           </div>
-          <h1 className="text-2xl font-medium">แจ้งปัญหา VAN</h1>
+          <h1 className="text-2xl font-medium">แจ้งปัญหา van</h1>
           <div className="flex flex-col items-center mt-[1rem] ">
             <h1>ชื่อผู้แจ้งปัญหา</h1>
-            <input className="p-1  bg-[#fff] border-2 border-cyan-400  outline-amber-400 rounded-lg" />
+            <input className="p-1  bg-[#fff] border-2 border-cyan-400  outline-amber-400 rounded-lg" onChange={(e) => setName(e.target.value)} value={name} />
           </div>
           <div className="flex flex-col items-center mt-[1rem] ">
             <h1>รหัส PS, MAS, VANSell</h1>
-            <input className="p-1  bg-[#fff] border-2 border-cyan-400  outline-amber-400 rounded-lg" />
+            <input className="p-1  bg-[#fff] border-2 border-cyan-400  outline-amber-400 rounded-lg" onChange={(e) => setPassword(e.target.value)} value={password} />
           </div>
           <div className="flex flex-col items-center mt-[1rem] ">
             <h1>บอกที่มาของปัญหา</h1>
-            <input className="p-8 bg-[#fff] border-2 border-cyan-400  outline-none rounded-lg" />
+            <input className="p-8 bg-[#fff] border-2 border-cyan-400  outline-none rounded-lg" onChange={(e) => setProblem(e.target.value)} value={problem} />
           </div>
           <div className="flex flex-col items-center mt-[1rem] ">
             <h1>อัปโหลดรูปภาพ</h1>
-            <button  type="file" className="bg-white border border-cyan-400   p-4 rounded-full">Upload</button>
+            <input type="file" onChange={(e) => setImg(URL.createObjectURL(e.target.files[0]))} />
           </div>
-          <button className='bg-cyan-400   text-[#fff] hover:bg-amber-400 hover:text-[#fff] p-2 mt-4 rounded-md flex items-center outline-none text-sm'>ยืนยัน</button>
+          <button className='bg-cyan-400 text-[#fff] hover:bg-amber-400 hover:text-[#fff] p-2 mt-4 rounded-md flex items-center outline-none text-sm' onClick={handleSubmission}>ยืนยัน</button>
         </div>
       </div>
-
-  
-        <Footer/>
+      <Footer />
     </div>
-    )
+  );
 }
